7d74a22823767802442e256e2e9b88cde9e29ce5
Hosam Hasan <hosamkora@gmail.com>
2020-04-03 17:25:26 +0200

    fix typo

--- lib/decorator/human.dart
+++ lib/decorator/human.dart
@@ -3,6 +3,6 @@ import 'package:design_patters_dart/decorator/wearable.dart';
 class Human implements Wearable {
   @override
   void wear() {
-    print('Human Wearing');
+    print('Human is Wearing');
   }
 }
